3ba0d8dac9c52ac59bddcea4187fca6eba0c1b9e
sotosergio88 <sotosergio88@gmail.com>
2016-01-18 19:20:57 -0600

    Update JavaApplication1.java

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -16,7 +16,7 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("primer programa de java 2");
+        System.out.println("dame mi punto");
     }
     
 }
